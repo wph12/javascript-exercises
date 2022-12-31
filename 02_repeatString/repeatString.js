65bc3cf6533c61b7a1815d53648ea4bd83e1fd6f
wph12 <peiheweng@gmail.com>
2022-12-31 08:26:11 +0800

    finish exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if (num < 0){
+        return "ERROR"
+    }
+    return str.repeat(num);
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split("");
+    let n = arr.length;
+    let arr2 = [];
+    for(i = 0; i < n; i++){
+        arr2[i] = arr[n-i-1];
+    }
+    return arr2.join("");
 };
 
 // Do not edit below this line
