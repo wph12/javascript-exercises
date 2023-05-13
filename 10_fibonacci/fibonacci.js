4920c8d1641f39aaed49a888981ae2d9c6ff1429
wph12 <peiheweng@gmail.com>
2023-05-13 11:37:22 +0800

    complete exercises up to 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let num = parseInt(input);
+    if(num){
+        if(num < 0){
+            return "OOPS"
+        }
+        else{
+            let arr = [];
+            arr[0] = 0;
+            arr[1] = 1;
+            for(let i = 2; i <= num; i++){
+                arr[i] = arr[i-1]+arr[i-2];
+            }
+            return arr[num];
+        }
+    }
+    else{
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    let oldestAge;
+    if(oldest.yearOfDeath){
+        oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    }
+    else{
+        oldestAge = new Date().getFullYear() - oldest.yearOfBirth;
+    }
 
+    for(let person of people){
+        let yearOfDeath;
+        if(person.yearOfDeath){
+            yearOfDeath = person.yearOfDeath;
+        }
+        else{
+            yearOfDeath = new Date().getFullYear()
+        }
+        let personAge = yearOfDeath - person.yearOfBirth;
+        if(personAge > oldestAge){
+            oldest = person;
+            oldestAge = personAge;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
